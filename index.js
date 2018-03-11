5f5048c2a512dfb7c43c07915c5233a58cfb3fb7
Santosh Rajan <santrajan@gmail.com>
2018-03-11 21:31:43 +0530

    case insensitive getHeader (#8)

--- index.js
+++ index.js
@@ -32,7 +32,8 @@ class Request {
   }
 
   getHeader (name) {
-    this.getAllHeaders().get(name)
+    name = name.toLowerCase()
+    return this.getAllHeaders().get(name)
   }
 }
 
@@ -342,7 +343,7 @@ function addAll (lens) {
 
 function indexHeaders (headers) {
   const map = new Map()
-  for (var i = 0; i < headers.length; i += 2) map.set(headers[i], headers[i + 1])
+  for (var i = 0; i < headers.length; i += 2) map.set(headers[i].toLowerCase(), headers[i + 1])
   return map
 }
 
