a5d482107521d728c58b82a0e7c356242bb7ba2e
Ashok <coderatlabs@gmail.com>
2018-03-26 12:47:04 +0530

    client: process request line and headers

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Server = require('./lib/server')
+const Client = require('./lib/client')
 
 exports.createServer = function (opts, onrequest) {
   if (typeof opts === 'function') return exports.createServer(null, opts)
@@ -7,4 +8,13 @@ exports.createServer = function (opts, onrequest) {
   const server = new Server(opts)
   if (onrequest) server.on('request', onrequest)
   return server
+}
+
+exports.request = function (obj) {
+  if (typeof obj !== 'object') return exports.request({
+    host: obj
+  })
+
+  const client = new Client(obj)
+  return client._connect()
 }
